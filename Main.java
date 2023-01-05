4c5e54a9c5bd75ccd6ff0da8942c71b81df71890
Your Name <you@example.com>
2023-01-05 11:17:20 +0700

    primeNumbersUnder100

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int i=1;
+        while (i<100)
+        {
+            if(isPrimeNumber(i))
+            {
+                System.out.println(i);
+            }
+            i=i+1;
+        }
+
+    }
+    public static boolean isPrimeNumber(int n) {
+        if (n < 2) {
+            return false;
+        }
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
